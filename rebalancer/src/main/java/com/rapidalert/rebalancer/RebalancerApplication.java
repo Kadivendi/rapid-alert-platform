87d23932f24f42a21334ea1c8f887b6bb5267ad2
Rohith Kadivendi <kv11@iitbbs.ac.in>
2026-01-23 20:11:00 -0700

    feat(rebalancer): add kafka DLQ rebalancer

---
+++ rebalancer/src/main/java/com/rapidalert/rebalancer/RebalancerApplication.java
@@ -0,0 +1,16 @@
+package com.rapidalert.rebalancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class RebalancerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RebalancerApplication.class, args);
+    }
+}
